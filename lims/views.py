15d4fc337af01d617019888d92ebebbc420eb9cf
Diki <dikibhuyan@gmail.com>
2019-07-27 11:11:55 +0530

    basic views made for lims part.. not done for user part

--- lims/views.py
+++ lims/views.py
@@ -1,3 +1,201 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter,SearchFilter
+from rest_framework.views import APIView
+from django.contrib.auth import login as django_login, logout as django_logout
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import *
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import mixins
 
-# Create your views here.
+class SectionView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=Section
+    serializer=SectionSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class TestView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=Test
+    serializer=TestSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class FieldView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=Field
+    serializer=FieldSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class ClientView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=Client
+    serializer=ClientSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class SampleView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=Sample
+    serializer=SampleSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class SampleTestView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=SampleTest
+    serializer=SampleTestSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class ResultFieldsView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    model=ResultFields
+    serializer=ResultFieldsSerializer
+    queryset = model.objects.all()
+    serializer_class = serializer
+    lookup_field='id'
+    filter_fields='__all__'
+    ordering_fields='__all__'
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
